5fbcb57bab166426f7ce2e4195ebe10d189ae7e6
simohamedhdafa <simohamed.hdafa@gmail.com>
2021-04-23 10:34:26 +0000

    chap4 added

---
+++ exercices/4/exo1.php
@@ -0,0 +1,6 @@
+<?php
+    $s = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut nostrum at vel enim velit reprehenderit magni debitis asperiores facilis vero? Repudiandae hic, quia corporis sint facilis molestias id? Cum veniam dolores dignissimos eveniet et debitis magnam ipsam repudiandae officiis libero.";
+
+    $s = ucwords($s);
+
+    echo $s;
---
+++ exercices/4/exo2.php
@@ -0,0 +1,6 @@
+<?php
+
+    $s = "PHP 5";
+
+    for($i=0; $i<strlen($s); $i++)
+        echo $s[$i] . "<br>";
